ff59b1899eb30c4e83cd3313c4f410042eb28375
ankit <ajha45075@gmail.com>
2026-01-05 12:54:15 +0530

    created chat model

---
+++ backend/src/models/chatModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ChatSchema = new mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Chat = mongoose.model("Chat", ChatSchema);
+
+module.exports = Chat;
