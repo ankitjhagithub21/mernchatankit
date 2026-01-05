e0c6501570cd79132dbe41fde452de06e44a821a
ankit <ajha45075@gmail.com>
2026-01-05 12:27:17 +0530

    added register and login routes

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,137 @@
+const User = require("../models/userModel");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const jwtSec = process.env.JWT_SECRET || "your_secret_key";
+
+const genToken = (payload) => {
+  return jwt.sign(payload, jwtSec, { expiresIn: "1d" });
+};
+
+const options = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "strict",
+  maxAge: 86400000, //1 day in miliseconds
+};
+
+const register = async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+
+    if (!fullname || !email || !password) {
+      return res
+        .status(400)
+        .json({ messsage: "All fields are required.", success: false });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({
+          messsage: "Please enter valid email address.",
+          success: false,
+        });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ messsage: "User already exist.", success: false });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res
+        .status(400)
+        .json({ messsage: "Please enter strong password.", success: false });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = genToken({ id: newUser._id });
+
+    res.cookie("token", token, options);
+
+    return res.status(201).json({
+      message: "Account created successfully.",
+      success: true,
+      user: {
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ messsage: "All fields are required.", success: false });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({
+          messsage: "Please enter valid email address.",
+          success: false,
+        });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ messsage: "User not found.", success: false });
+        
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if(!isValidPassword){
+        return res.status(400).json({message:"Wrong credentials", success:false})
+    }
+
+
+    const token = genToken({id:user._id});
+
+    res.cookie('token', token, options)
+
+
+    return res.status(200).json({
+        message:`Welcome back ${user.fullname}`,
+        success:true,
+        user:{
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email
+        }
+    })
+    
+
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+
+module.exports = {
+    register, 
+    login
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,28 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 const app = express()
 const port = process.env.PORT || 8000
 
-connectDB()
+
+app.use(express.json())
+app.use(cors({
+  origin:process.env.ORIGIN || "http://localhost:5173",
+  credentials:true
+}))
+app.use(cookieParser())
+
 
 app.get('/', (req, res) => {
   res.json({message:"ok"})
 })
 
+app.use("/api/auth", require("./routes/authRoutes"))
+
+
 app.listen(port, () => {
+  connectDB()
   console.log(`Server is listening on port ${port}`)
 })
